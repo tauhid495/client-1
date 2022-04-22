8ee678b0a796ebacb92ce4721e65be5505230a4e
tauhid <tauhid495@gmail.com>
2022-04-22 17:34:38 +0600

    hi

--- src/App.js
+++ src/App.js
@@ -1,53 +1,20 @@
-import { useState, useEffect } from 'react';
+
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import AddUser from './conmponent/addUsser/AddUser';
+import Home from './conmponent/home/Home';
 
 function App() {
-  const [users, setUsers] = useState([]);
 
-  // load data
-  useEffect(() => {
-    fetch('http://localhost:5000/users')
-      .then(res => res.json())
-      .then(data => setUsers(data));
-  }, [])
-
-  const handleAddUser = event => {
-    event.preventDefault();
-    const name = event.target.name.value;
-    const email = event.target.email.value;
-    const user = { name, email };
-
-    // post data to server
-
-    fetch('http://localhost:5000/user', {
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json'
-      },
-      body: JSON.stringify(user)
-    })
-      .then(res => res.json())
-      .then(data => {
-        const newUsers = [...users, data];
-        setUsers(newUsers);
-        console.log(data);
-      })
-
-  }
 
   return (
     <div className="App">
-      <h1>My Own data: {users.length}</h1>
-      <form onSubmit={handleAddUser}>
-        <input type="text" name="name" placeholder='Name' required />
-        <input type="text" name="email" placeholder='Email' required />
-        <input type="submit" value="Add User" />
-      </form>
-      <ul>
-        {
-          users.map(user => <li key={user.id}>id: {user.id} name: {user.name} email: {user.email}</li>)
-        }
-      </ul>
+      <Routes>
+        <Route path='user/add' element={<AddUser />} ></Route>
+        <Route path='/home' element={<Home />} ></Route>
+
+
+      </Routes>
     </div>
   );
 }
